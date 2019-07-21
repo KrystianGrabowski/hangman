fd5611feda6b836fd00b9c4430d13452f53ceb96
KrystianGrabowski <krystian987543@gmail.com>
2019-07-21 18:24:40 +0200

    Alphabet

--- hangman.js
+++ hangman.js
@@ -2,6 +2,7 @@ var sentence = "ala ma kota";
 var length = sentence.length; 
 sentence = sentence.toUpperCase();
 var userSentence = createSentenceToShow();
+var alphabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ";
 
 function createSentenceToShow(){
     var userSentence = "";
@@ -15,4 +16,18 @@ function showSentence(){
     document.getElementById("sentence").innerHTML = userSentence;
 }
 
-window.onload = showSentence;
+function showKeyboard(){
+    var keyBoardStructure = " ";
+    for(i=0; i<35; i++){
+        keyBoardStructure += '<div class="character">' + alphabet.charAt(i) + '</div>';
+        if ((i+1) % 7 == 0) keyBoardStructure += '<div style="clear: both"></div>';  
+    }
+    document.getElementById("alphabet").innerHTML = keyBoardStructure;
+}
+
+function start(){
+    showSentence();
+    showKeyboard();
+}
+
+window.onload = start;
