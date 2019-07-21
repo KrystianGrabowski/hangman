21933e247f6eafe513d0d2be4f64777d0df7c47f
KrystianGrabowski <krystian987543@gmail.com>
2019-07-21 17:32:10 +0200

    Image and sentence

--- hangman.js
+++ hangman.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+var sentence = "ala ma kota";
+var length = sentence.length; 
+sentence = sentence.toUpperCase();
+var userSentence = createSentenceToShow();
+
+function createSentenceToShow(){
+    var userSentence = "";
+    for(i=0; i<length; i++){
+        userSentence += sentence.charAt(i) == " " ? " " : "-"; 
+    }
+    return userSentence;
+}
+
+function showSentence(){
+    document.getElementById("sentence").innerHTML = userSentence;
+}
+
+window.onload = showSentence;
