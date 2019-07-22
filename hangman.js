668adaf6a77694c489b8d2108ac8c635e42f1812
KrystianGrabowski <krystian987543@gmail.com>
2019-07-22 12:44:41 +0200

    End screen + effects

--- hangman.js
+++ hangman.js
@@ -4,6 +4,7 @@ sentence = sentence.toUpperCase();
 var userSentence = createSentenceToShow();
 var alphabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ";
 var imageNumber = 0;
+const MAX_IMAGE_NUMBER = 8; 
 
 function createSentenceToShow(){
     var userSentence = "";
@@ -20,8 +21,8 @@ function showSentence(){
 function showKeyboard(){
     var keyBoardStructure = " ";
     for(i=0; i<35; i++){
-        keyBoardStructure += '<div class="character" onclick="checkIfCharacterExists(' + 
-                                i + ')">' + alphabet.charAt(i) + '</div>';
+        keyBoardStructure += "<div class='character' onclick='checkIfCharacterExists(" + 
+                                i + ")' id='key" + i + "'>" + alphabet.charAt(i) + "</div>";
         if ((i+1) % 7 == 0) keyBoardStructure += '<div style="clear: both"></div>';  
     }
     document.getElementById("alphabet").innerHTML = keyBoardStructure;
@@ -32,30 +33,59 @@ String.prototype.replaceCharacter = function(position, character){
     return this.substr(0, position) + character + this.substr(position + 1);
 }
 
-function FadeImage(){
-    
+function insertHTML(index, message){
+    document.getElementById(index).innerHTML = message;
+    $("#alphabet").fadeIn(3000);
 }
 
-function Lose(){
-    
+function FadeElement(name){
+    $("#" + name).fadeOut(2000);
 }
 
 function checkLose(){
-    
-}
-
-function Win(){
-    
+    if (imageNumber == MAX_IMAGE_NUMBER){
+        lockAllKeys();
+        FadeElement("alphabet");
+        setTimeout("insertHTML('alphabet', 'Przegrana!<br/>Prawidłowe hasło to:<br/>" + sentence + "')", 2000);
+    }
 }
 
 function checkWin(){
-
+    if (sentence == userSentence){
+        lockAllKeys();
+        FadeElement("alphabet");
+        setTimeout("insertHTML('alphabet', 'Gratulacje!<br/>Podano prawidłowe hasło')", 2000);
+    }
 }
 
 function nextImage(){
     imageNumber++;
     document.getElementById("hangmanImage").innerHTML = "<img src='images/image" + imageNumber + ".png' width='500' alt=''/>"
     checkLose();
+}
+
+function lockKey(charNum){
+    var keyId = "key" + charNum;
+    document.getElementById(keyId).setAttribute("onclick", ";");
+    document.getElementById(keyId).style.cursor = "default";
+}
+
+function keyCorrectAnswer(charNum){
+    var keyId = "key" + charNum;
+    document.getElementById(keyId).style.background = "green";
+    lockKey(charNum);
+}
+
+function keyWrongAnswer(charNum){
+    var keyId = "key" + charNum;
+    document.getElementById(keyId).style.background = "red";
+    lockKey(charNum);
+}
+
+function lockAllKeys(){
+    for(i=0; i<35; i++){
+        lockKey(i);   
+    }
 }
 
 function checkIfCharacterExists(charNum){
@@ -68,7 +98,12 @@ function checkIfCharacterExists(charNum){
             checkWin();
         }
     }
-    if (!characterFound) nextImage();
+    if (!characterFound){
+        keyWrongAnswer(charNum);
+        nextImage();
+    } else{
+        keyCorrectAnswer(charNum);
+    }
     showSentence();
 }
 
